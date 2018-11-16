4e59a91307bc5e6e7b6deca97d3c03f8b609a3b7
Liv <livsisinilsson@gmail.com>
2018-11-16 13:06:30 +0100

    endringer
    satt på lyd når man trykker på tastaturet
    lagt inn videoer

---
+++ IT2/jul_prosjekt/script.js
@@ -0,0 +1,26 @@
+console.log("Skriptet er lastet");
+
+window.onkeydown = function(event){
+    console.log(event.code);
+    const boks = document.querySelector(`div[data-code=${event.code}]`);
+    const lyd = document.querySelector(`audio[data-code=${event.code}]`);
+
+    if(!lyd){
+        return;
+    }
+
+    lyd.currentTime = 0;
+    lyd.play();
+
+    boks.classList.add("spiller");
+
+}
+window.onkeyup = function(event){
+    const boks = document.querySelector(`div[data-code=${event.code}]`);
+
+    if(!boks){
+        return;
+    }
+
+    boks.classList.remove("spiller");
+}
--- IT2/uke44/trommemaskin/script.js
+++ IT2/uke44/trommemaskin/script.js
@@ -1,10 +1,11 @@
 console.log("Skriptet er lastet");
 
 window.onkeydown = function(event){
+    console.log(event.code);
     const boks = document.querySelector(`div[data-code=${event.code}]`);
     const lyd = document.querySelector(`audio[data-code=${event.code}]`);
 
-    if(!boks){
+    if(!lyd){
         return;
     }
 
